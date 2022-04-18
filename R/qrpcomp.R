ace8d3259c86ad39923d4f780a6065453591cdc9
Rafael S. de Souza <rafael.2706@gmail.com>
2022-04-18 14:35:11 +0800

    add gpu

--- R/qrpcomp.R
+++ R/qrpcomp.R
@@ -18,6 +18,8 @@
 #' @param scale a logical value indicating whether the columns of \code{x}
 #'   should be scaled to have unit variance before the analysis takes place. The
 #'   default is \code{FALSE} for consistency with \code{prcomp}.
+#'  @param cuda a logical value indicating whether cuda acceleration should be
+#'  used. The default is \code{FALSE}.
 #' @return \code{qrprcomp} returns a list with class \code{prcomp}
 #'   containing the following elements: \item{sdev}{the additional standard
 #'   deviation explained by each component.}
@@ -28,28 +30,34 @@
 #'   \item{center, scale}{the centering and
 #'   scaling used, or \code{FALSE}}
 #' @export
-qrprcomp <- function(x,center = TRUE, scale = FALSE){
-    x <- scale(x, center = center, scale = scale)
-    cen <- attr(x, "scaled:center")
-    sc <- attr(x, "scaled:scale")
-    x0 <- if (is_torch_dtype(x)) x  else torch_tensor(as.matrix(x))
-    out = torch::torch_qr(x0)
-    q <- out[[1]]
-    r <- out[[2]]
-    A <- torch::torch_svd(r$t())
-    u =  A[[1]]
-    s =  torch::torch_diag(A[[2]])
-    v = A[[3]]
-    out <- torch::torch_mm(torch_mm(q,v),s)
-    sdev <- as.numeric(A[[2]]/sqrt(max(1, nrow(x) - 1)))
-    rotation <- as.matrix(u)
-    dimnames(rotation) <- list(colnames(x), paste0("PC", seq_len(ncol(u))))
-    out <- as.matrix(out)
-    colnames(out) <- colnames(rotation)
-    r <- list(sdev = sdev, rotation = rotation,
-    center = if(is.null(cen)) FALSE else cen,
-    scale = if(is.null(sc)) FALSE else sc,
-    x = as.matrix(out))
-    class(r) <- "prcomp"
-    r
+qrprcomp <- function(x,center = TRUE, scale = FALSE,cuda = FALSE){
+  if(cuda == TRUE){
+    if(cuda_is_available()){device = torch_device("cuda:0")} else
+      device = torch_device(type='cpu')
   }
+  else
+    x <- scale(x, center = center, scale = scale)
+  cen <- attr(x, "scaled:center")
+  sc <- attr(x, "scaled:scale")
+  x0 <- if (is_torch_dtype(x)) x  else torch_tensor(as.matrix(x),device=device)
+  out = torch::torch_qr(x0)
+  q <- out[[1]]
+  r <- out[[2]]
+  A <- torch::torch_svd(r$t())
+  u =  A[[1]]
+  s =  torch::torch_diag(A[[2]])
+  v = A[[3]]
+  out <- torch::torch_mm(torch_mm(q,v),s)
+  sved <- A[[2]]/sqrt(max(1, nrow(x) - 1))
+  sdev <- as.numeric(sved$cpu())
+  rotation <- as.matrix(u$cpu())
+  dimnames(rotation) <- list(colnames(x), paste0("PC", seq_len(ncol(u))))
+  out <- as.matrix(out$cpu())
+  colnames(out) <- colnames(rotation)
+  r <- list(sdev = sdev, rotation = rotation,
+            center = if(is.null(cen)) FALSE else cen,
+            scale = if(is.null(sc)) FALSE else sc,
+            x = as.matrix(out))
+  class(r) <- "prcomp"
+  r
+}
